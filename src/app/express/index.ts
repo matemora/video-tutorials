cea0d0464a99af8021c9b9010d4f6f70d49be6e0
Mateus Morais <mateus.morais@eduq.app>
2022-08-21 17:21:07 -0300

    comment unused routes to run app

--- src/app/express/index.ts
+++ src/app/express/index.ts
@@ -3,7 +3,7 @@ import { join } from 'path';
 import { ENV } from '../../env';
 
 import mountMiddleware from './mount-middleware';
-import mountRoutes from './mount-routes';
+// import mountRoutes from './mount-routes';
 
 type CreateExpressAppParams = {
   config: {
@@ -19,7 +19,7 @@ export default function createExpressApp({ config, env }: CreateExpressAppParams
   app.set('view engine', 'pug');
 
   mountMiddleware(app, env);
-  mountRoutes(app, config);
+  // mountRoutes(app, config);
 
   return app
 }
